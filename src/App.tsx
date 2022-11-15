f9b33bf4a3c65e0ca2ce8fef8a09de50d6538c32
Хикмет Садигзаде <sadigzade.hi@mail.ru>
2022-11-15 11:44:20 +0300

    Created a Header component

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
 import MonthStatistics from './pages/MonthStatistics';
+import Header from './shared/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/month-statistics" element={<MonthStatistics />} />
